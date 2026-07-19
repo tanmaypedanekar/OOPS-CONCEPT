2320c029833c180cded245c41e8cb9894c27e39f
TANMAY TUKARAM PEDANEKAR <tanmaypedanekar.cse@gmail.com>
2026-07-19 09:58:07 +0530

    Add files via upload
    Library Management System.

---
+++ libraryManagement.java
@@ -0,0 +1,46 @@
+public class libraryManagement {
+    // instance variable
+    static int totalBooks = 122;
+    String title;
+    String author;
+    boolean isBorrowed;
+
+    // paramiterized constructor;
+
+    libraryManagement(String title) {
+        this.title = title; // this keyword gives the first prefrance to instance varible title and store
+                            // the value in it.
+        author = "abdul kalam";
+
+    }
+
+    // instance methoods
+    public void borrowBook() {
+        System.out.println("WELCOME TO BORROW BOOK MECHANISM: ");
+        if (isBorrowed) {
+            System.out.println("this book is already borwwoed");
+        } else {
+            isBorrowed = true;
+            System.out.println("enjoy the book");
+        }
+    }
+
+    public void returnBook() {
+
+        System.out.println("WELCOME TO RETURN BOOK MECHANISM: ");
+        if (isBorrowed) {
+            isBorrowed = false;
+            System.out.println("return completed");
+
+        } else {
+            System.out.println("The book is already in the library");
+        }
+    }
+
+    public static void main(String[] args) {
+        libraryManagement book = new libraryManagement("Tanmay");
+        book.borrowBook();
+        book.returnBook();
+    }
+
+}
