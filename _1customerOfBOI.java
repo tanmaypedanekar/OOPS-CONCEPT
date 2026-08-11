6862485c9bb7c0e53eb2451122ce61c68c7240cf
tanmaypedanekar <tanmaypedanekar.cse@gmail.com>
2026-08-11 22:19:33 +0530

    Add a class of customer of the BOI

---
+++ _1customerOfBOI.java
@@ -0,0 +1,9 @@
+public class _1customerOfBOI {
+    public static void main(String[] args) {
+
+        _1bankOfIndia customer = new _1bankOfIndia();
+        customer.deposit();
+        customer.withdrawal();
+        customer.checkBalance();
+    }
+}
