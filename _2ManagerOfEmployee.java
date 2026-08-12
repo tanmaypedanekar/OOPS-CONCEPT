127ad1ee34df00991ed800b7cd4f4961b27bc49e
tanmaypedanekar <tanmaypedanekar.cse@gmail.com>
2026-08-12 07:33:37 +0530

    Add class for calling the employee program

---
+++ _2ManagerOfEmployee.java
@@ -0,0 +1,6 @@
+public class _2ManagerOfEmployee {
+    public static void main(String[] args) {
+        _2employee details = new _2employee("tanmay", 20, 200000, true);
+        details.displayDetails();
+    }
+}
