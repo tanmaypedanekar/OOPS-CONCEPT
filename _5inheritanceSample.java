5a64f621b59b6217416eddc572670ec05b0b613d
tanmaypedanekar <tanmaypedanekar.cse@gmail.com>
2026-08-13 21:56:28 +0530

    Add simple program for inheritance

---
+++ _5inheritanceSample.java
@@ -0,0 +1,13 @@
+public class _5inheritanceSample {
+    int num ;
+    String name;
+    Boolean fact;
+
+    public void printStatement(){
+        System.out.println("Everything is good");
+    }
+
+    public int addition(int a,int b){
+        return a+b;
+    }
+}
