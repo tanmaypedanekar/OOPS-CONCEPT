a2a75c4c4a2081076bb81666061c60c89eaed9a6
tanmaypedanekar <tanmaypedanekar.cse@gmail.com>
2026-08-13 21:57:55 +0530

    Add program to check inheritance

---
+++ _5inheritanceSample2.java
@@ -0,0 +1,7 @@
+public class _5inheritanceSample2 extends _5inheritanceSample {
+    public static void main(String[] args) {
+        _5inheritanceSample sam = new _5inheritanceSample();
+        sam.printStatement();
+        System.out.println(sam.addition(20,20));
+    }
+}
